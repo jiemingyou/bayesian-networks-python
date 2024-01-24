1ae3fc67671b41c96dac68db9cbd2c5c24b6f5ae
Jieming You <jieming.you@gmail.com>
2024-01-24 15:17:53 +0200

    feat: check d-separation

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # bayesian-networks-python
-Elementary Bayesian Network abstraction for the ML: Advanced Probability Methods course
+
+Elementary Bayesian Network abstraction for the ML: Advanced Probability Methods
+course
+
+## Example
+
+Assessing the conditional independence between $C \perp\!\!\!\perp E \mid B, D$
+given the following Bayesian Network:
+
+![BN](assets/markov.png)
+
+```python
+from src.network import BayesianNetwork
+
+# Initiate the network
+bn = BayesianNetwork()
+
+# Add the nodes
+bn.add_node("A")
+bn.add_node("B")
+bn.add_node("C")
+bn.add_node("D")
+bn.add_node("E")
+bn.add_node("F")
+
+# Add the directed edges
+bn.add_edge("A", "C")
+bn.add_edge("A", "D")
+bn.add_edge("A", "F")
+bn.add_edge("B", "C")
+bn.add_edge("B", "E")
+bn.add_edge("C", "D")
+bn.add_edge("D", "E")
+bn.add_edge("F", "E")
+
+# Calculate the d-separation
+bn.is_d_separated("C", "E", ["B", "D"])
+
+# Returns: (False , {unblocked set})
+```
--- src/network.py
+++ src/network.py
@@ -12,6 +12,13 @@ class BayesianNetwork:
     def __repr__(self):
         return f"Nodes: {self.nodes}\nEdges: {self.edges}"
 
+    def not_in_list(self, list1: list, list2: list) -> bool:
+        """Check if list1 is not in list2."""
+        for item in list1:
+            if item in list2:
+                return False
+        return True
+
     def add_node(self, node: str) -> None:
         """Add a node to the network."""
         self.nodes.add(node)
@@ -70,4 +77,50 @@ class BayesianNetwork:
         return paths
 
     def is_collider(self, node1: str) -> bool:
-        pass
+        """Check if a node is a collider."""
+        return len(self.get_parents(node1)) >= 2
+
+    def is_blocked(self, path: list, cond: list) -> bool:
+        """Check if a path between node1 and node2 is blocked."""
+        Z = set(cond)
+        for w in path:
+            if self.is_collider(w):
+                if w not in Z and self.not_in_list(self.get_parents(w), Z):
+                    return True
+            else:
+                if w in Z:
+                    return True
+        return False
+
+    def is_d_separated(self, node1: str, node2: str, cond: list) -> bool:
+        """Check if node1 and node2 are d-separated given cond."""
+        for path in self.get_paths(node1, node2):
+            if not self.is_blocked(path, cond):
+                return False, path
+        return True
+
+
+if __name__ == "__main__":
+    bn = BayesianNetwork()
+    bn.add_node("A")
+    bn.add_node("B")
+    bn.add_node("C")
+    bn.add_node("D")
+    bn.add_node("E")
+    bn.add_node("F")
+
+    bn.add_edge("A", "C")
+    bn.add_edge("A", "D")
+    bn.add_edge("A", "F")
+    bn.add_edge("B", "C")
+    bn.add_edge("B", "E")
+    bn.add_edge("C", "D")
+    bn.add_edge("D", "E")
+    bn.add_edge("F", "E")
+
+    print(bn.is_d_separated("A", "B", ["C"]))
+    print(bn.is_d_separated("A", "B", []))
+    print(bn.is_d_separated("C", "E", ["B", "D"]))
+    print(bn.is_d_separated("C", "D", ["A", "B"]))
+    print(bn.is_d_separated("B", "F", ["A", "C"]))
+    print(bn.is_d_separated("A", "E", ["D", "F"]))
