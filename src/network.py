7d05d4df77044feb6b9f96fcf3ac7c024849955f
Jieming You <jieming.you@gmail.com>
2024-01-25 17:22:35 +0200

    fix: replace list iteration with set operations

--- src/network.py
+++ src/network.py
@@ -12,13 +12,6 @@ class BayesianNetwork:
     def __repr__(self):
         return f"Nodes: {self.nodes}\nEdges: {self.edges}"
 
-    def not_in_list(self, list1: list, list2: list) -> bool:
-        """Check if list1 is not in list2."""
-        for item in list1:
-            if item in list2:
-                return False
-        return True
-
     def add_node(self, node: str) -> None:
         """Add a node to the network."""
         self.nodes.add(node)
@@ -30,33 +23,38 @@ class BayesianNetwork:
         """
         self.edges.add((node1, node2))
 
-    def get_parents(self, node: str) -> list:
+    def get_parents(self, node: str) -> set[str]:
         """Get the parents of a node."""
-        parents = []
+        parents = set()
         for edge in self.edges:
             if edge[1] == node:
-                parents.append(edge[0])
+                parents.add(edge[0])
         return parents
 
-    def get_children(self, node: str) -> list:
+    def get_children(self, node: str) -> set[str]:
         """Get the children of a node."""
-        children = []
+        children = set()
         for edge in self.edges:
             if edge[0] == node:
-                children.append(edge[1])
+                children.add(edge[1])
         return children
 
-    def get_neighbors(self, node: str) -> list:
+    def get_neighbors(self, node: str) -> set[str]:
         """Get the neighbors of a node."""
-        neighbors = []
+        neighbors = set()
         for edge in self.edges:
             if edge[0] == node:
-                neighbors.append(edge[1])
+                neighbors.add(edge[1])
             elif edge[1] == node:
-                neighbors.append(edge[0])
+                neighbors.add(edge[0])
         return neighbors
 
-    def get_paths(self, node1: str, node2: str, visited: set = None) -> list:
+    def get_paths(
+        self,
+        node1: str,
+        node2: str,
+        visited: set[str] = None,
+    ) -> list[list[str]]:
         """
         Get all paths from node1 to node2.
         Treat the network as an undirected graph.
@@ -80,19 +78,27 @@ class BayesianNetwork:
         """Check if a node is a collider."""
         return len(self.get_parents(node1)) >= 2
 
-    def is_blocked(self, path: list, cond: list) -> bool:
+    def set_intersection(self, set1: set[str], set2: set[str]) -> bool:
+        """Check if two sets have intersection."""
+        return len(set1.intersection(set2)) > 0
+
+    def is_blocked(self, path: list, Z: set[str]) -> bool:
         """Check if a path between node1 and node2 is blocked."""
-        Z = set(cond)
         for w in path:
             if self.is_collider(w):
-                if w not in Z and self.not_in_list(self.get_parents(w), Z):
+                if w not in Z and not self.set_intersection(self.get_parents(w), Z):
                     return True
             else:
                 if w in Z:
                     return True
         return False
 
-    def is_d_separated(self, node1: str, node2: str, cond: list) -> bool:
+    def is_d_separated(
+        self,
+        node1: str,
+        node2: str,
+        cond: set[str],
+    ) -> bool:
         """Check if node1 and node2 are d-separated given cond."""
         for path in self.get_paths(node1, node2):
             if not self.is_blocked(path, cond):
@@ -118,9 +124,9 @@ if __name__ == "__main__":
     bn.add_edge("D", "E")
     bn.add_edge("F", "E")
 
-    print(bn.is_d_separated("A", "B", ["C"]))
-    print(bn.is_d_separated("A", "B", []))
-    print(bn.is_d_separated("C", "E", ["B", "D"]))
-    print(bn.is_d_separated("C", "D", ["A", "B"]))
-    print(bn.is_d_separated("B", "F", ["A", "C"]))
-    print(bn.is_d_separated("A", "E", ["D", "F"]))
+    print("a)", bn.is_d_separated("A", "B", {"C"}))
+    print("b)", bn.is_d_separated("A", "B", {}))
+    print("c)", bn.is_d_separated("C", "E", {"B", "D"}))
+    print("d)", bn.is_d_separated("C", "D", {"A", "B"}))
+    print("e)", bn.is_d_separated("B", "F", {"A", "C"}))
+    print("f)", bn.is_d_separated("A", "E", {"D", "F"}))
